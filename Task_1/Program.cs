57e023d63195281fcda07c5507198a63c8131bc6
Galina <kuzmina0497@mail.ru>
2022-05-01 10:59:26 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Первое число равно: " + a);
+
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Второе число равно: " + b);
+
+int max = 1;
+ 
+ if (a > b) 
+ max = a;
+ else 
+ max = b;
+
+ Console.WriteLine ("Максимальное значение равно " + max);
