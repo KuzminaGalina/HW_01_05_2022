2953660e4d49a8ccc40a612bad35341ee8608787
Galina <kuzmina0497@mail.ru>
2022-05-01 11:48:37 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите количество чисел ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int max = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= n; i++)
+{
+    Console.WriteLine("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number > max) max = number;
+}
+Console.WriteLine("Максимальное число: " + max);
