be978b980c6f106c83eb492a94c2d9781745cee9
Galina <kuzmina0497@mail.ru>
2022-05-01 16:57:20 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= n; i = i + 2)
+{
+    Console.Write(i);
+    if(i < n-1)
+    {
+        Console.Write(",");
+    }
+}
+
